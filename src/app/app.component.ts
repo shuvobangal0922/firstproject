26a9c28d0b20f873a9f9ee8a1c24bf0f06dd549e
shuvobangal0922 <shuvo.bangalxx22@gmail.com>
2023-09-22 16:28:38 +0530

    Template driven form completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,9 @@ import { Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Lecture 36: Custom pipes ';
-  
+  title = 'Lecture 38: Forms';
+  userLogin(item:any){
+    console.warn(item);
+  }
   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UsdInrPipe } from './pipes/usd-inr.pipe';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsdInrPipe,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule
    
   ],
   providers: [],
