00cf103a6f35de2c7d304ad4d0d6ce6951ab7670
shuvobangal0922 <shuvo.bangalxx22@gmail.com>
2023-09-25 11:56:11 +0530

    Forms validation done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms'
+import {FormControl, FormGroup, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -9,12 +9,19 @@ import {FormControl, FormGroup} from '@angular/forms'
 export class AppComponent {
   title = 'Lecture 40: Reactive form';
   loginForm=new FormGroup({
-    username:new FormControl(''),
-    password:new FormControl('')
+    username:new FormControl('',[Validators.required, Validators.pattern('[a-z A-Z]+$')]),
+    password:new FormControl('', [Validators.required, Validators.minLength(8)])
   })
   userLogin(){
     console.warn(this.loginForm.value);
     
   }
-  
+  get username(){
+    return this.loginForm.get('username');
+  }
+  get password(){
+    return this.loginForm.get('password');
+  }
+
 }
+  
