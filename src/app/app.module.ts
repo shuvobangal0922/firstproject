3e159cdcbeb2fac2a3be2ad77739d690732dd174
shuvobangal0922 <shuvo.bangalxx22@gmail.com>
2023-09-28 11:39:41 +0530

    Routing module completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {AdminModule} from './admin/admin.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -13,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    
+    AdminModule
    
   ],
   providers: [],
